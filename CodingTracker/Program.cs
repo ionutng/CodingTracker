f0ed02f4959bf0b318ea52356038da88b0bd3dd6
ionutng <ionutngp@gmail.com>
2023-09-28 10:50:47 +0300

    Add configuration file

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Configuration;
+using System.Collections.Specialized;
+
+// Read all the keys from the config file
+NameValueCollection sAll;
+sAll = ConfigurationManager.AppSettings;
+
+foreach (string s in sAll.AllKeys)
+    Console.WriteLine("Key: " + s + " Value: " + sAll.Get(s));
